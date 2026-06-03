39e3fb81d05694e63d9856b19dfbe247c518d718
Zaur Kurbanismailov <Kurbanismailov.92@gmail.com>
2026-06-03 17:33:32 +0300

    Update uniject

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Uniject;
 using UnityEngine;
 
@@ -5,20 +6,27 @@ public class Test : MonoBehaviour
 {
     [SerializeField] private Character _characterPrefab;
 
-    private void Start()
+    private IEnumerator Start()
     {
         // Container.Bind<Contract>().To<Concrete>().From*().AsScope().NonLazy();
-
         // Container.Bind<IPlayer>().To<IPlayer>().FromNew().AsTransient();
+
+        // Container.Bind<GameObject>().From
+        // Container.Bind<Character>().From
+        // Container.Bind<ICharacter>().From
 
         var container = new Container();
         container.Bind<ICharacter>().To<Character>().FromComponentInNewPrefab(_characterPrefab).AsCached().NonLazy();
         container.Bind<Enemy>();
 
-        // container.Resolve<Character>().Move();
+        container.Bind<GameObject>().To<GameObject>().FromComponentInNewPrefab(_characterPrefab.gameObject);
+        container.Bind<Character>().To<Character>().FromComponentInNewPrefab(_characterPrefab);
+        container.Bind<ICharacter>().To<ICharacter>().FromComponentInNewPrefab(_characterPrefab);
+
+        container.Resolve<ICharacter>().Move();
         container.Resolve<ICharacter>().Move();
         Debug.Log("Instantiating Character prefab...");
-        // container.Resolve<ICharacter>().Move();
-        // container.Instantiate<Character>().Move();
+
+        yield return null;
     }
 }
