2b799d2fe169453272d3273df7d534a43a4f082b
Zaur Kurbanismailov <kurbanismailov.z@azurgames.com>
2026-06-06 16:32:43 +0300

    Update Uniject

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Uniject;
+using Uniject.Tests;
 using UnityEngine;
 
 public class Test : MonoBehaviour
@@ -9,28 +10,9 @@ public class Test : MonoBehaviour
     private IEnumerator Start()
     {
         // Container.Bind<Contract>().To<Concrete>().From*().AsScope().NonLazy();
-        // Container.Bind<IPlayer>().To<IPlayer>().FromNew().AsTransient();
-
-        // Container.Bind<GameObject>().From
-        // Container.Bind<Character>().From
-        // Container.Bind<ICharacter>().From
 
         var container = new Container();
-        // container.Bind<ICharacter>().To<Character>().FromComponentInNewPrefab(_characterPrefab).AsCached().NonLazy();
-        container.Bind<Enemy1>();
-        container.Bind<Enemy2>();
-        container.Bind<Enemy3>();
-
-        container.ResolveNonLazyBindings();
-
-        // container.Bind<GameObject>().To<GameObject>().FromComponentInNewPrefab(_characterPrefab.gameObject);
-        // container.Bind<Character>().To<Character>().FromComponentInNewPrefab(_characterPrefab);
-        // container.Bind<ICharacter>().To<ICharacter>().FromComponentInNewPrefab(_characterPrefab);
-
-        container.Resolve<Enemy1>();
-        // container.Resolve<ICharacter>().Move();
-        // container.Resolve<ICharacter>().Move();
-        Debug.Log("Instantiating Character prefab...");
+        container.Bind<Class>().To<Class>().FromComponentInNewPrefab(new GameObject());
 
         yield return null;
     }
